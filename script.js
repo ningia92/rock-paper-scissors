fa2f0649036f2a099d48b35ce9476968cc4ff9be
ningia <113545072+ningia92@users.noreply.github.com>
2025-04-24 12:33:44 +0200

    feat: add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+const getComputerChoice = () => {
+  const choice = Math.floor(Math.random() * 3) + 1
+  
+  if (choice === 1) {
+    return 'rock'
+  } else if (choice === 2) {
+    return 'paper'
+  } else {
+    return 'scissors'
+  }
+}
